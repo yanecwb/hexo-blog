aefda43c8c84260979db4bddf2440fccffbfd8dc
chenwenbin <chenwenbin@qq.com>
2023-02-23 10:03:13 +0800

    FPS/统计访问

--- themes/next/source/js/diy-footer.js
+++ themes/next/source/js/diy-footer.js
@@ -1,8 +1,39 @@
 // 动态心跳
 $(document).ready(function(e){
     // $('.copyright').html('©2021 <i class="fa-fw fas fa-heart card-announcement-animation cc_pointer"></i> By 花猪');
+
+    // FPS
+    (function(){
+        var requestAnimationFrame =
+            window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function(callback) {
+                window.setTimeout(callback, 1000/60);
+            };
+        var fps,last,offset,step,appendFps;
+
+        fps = 0;
+        last = Date.now();
+        step = function(){
+            offset = Date.now() - last;
+            fps += 1;
+            if( offset >= 1000 ){
+            last += offset;
+            appendFps(fps);
+            fps = 0;
+            }
+            requestAnimationFrame( step );
+        };
+        appendFps = function(fps){
+            document.querySelector('#fps').innerHTML = fps+'FPS'
+        };
+        step();
+    })();
     $('.footer')[0].style.position = 'absolute'
-    $('.footer')[0].style.bottom = '-130px'
+    $('.footer')[0].style.bottom = '-150px'
     $('.footer')[0].style.width = '100%'
 })
 
